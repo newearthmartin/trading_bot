bda7b5cb7f84b8ed3c304083faedd918747bd80e
Martín Massera <martinmassera@gmail.com>
2021-03-12 12:35:30 -0300

    special script for saving msgs

--- binance_manager.py
+++ binance_manager.py
@@ -6,28 +6,21 @@ from order_trade import Trade
 
 
 class BinanceManager:
-    def __init__(self, trade_listener=None, second_listener=None):
+    def __init__(self, trade_listener=None, second_listener=None, msg_listener=None):
         self.current_second = None
         self.current_second_trades = []
         self.client = Client(settings.BINANCE_API_KEY, settings.BINANCE_SECRET_KEY)
-        self.trade_listeners = []
-        self.second_listeners = []
-        if trade_listener:
-            self.trade_listeners.append(trade_listener)
-        if second_listener:
-            self.second_listeners.append(second_listener)
-
-        self.out_file = open('msgs.txt', 'a')
+        self.msg_listeners = [msg_listener] if msg_listener else []
+        self.trade_listeners = [trade_listener] if trade_listener else []
+        self.second_listeners = [second_listener] if second_listener else []
 
     def start(self):
         self.bm = BinanceSocketManager(self.client)
-        self.bm.start_aggtrade_socket('BTCUSDT', lambda msg: self.process_message(msg))
+        self.bm.start_aggtrade_socket('BTCUSDT', self.process_message)
         self.bm.start()
         print('Binance manager started...')
 
     def process_message(self, msg):
-        self.out_file.write(json.dumps(msg) + '\n')
-        self.out_file.flush()
         ts_E = msg['E']
         price = float(msg['p'])
         qty = float(msg['q'])
@@ -47,8 +40,23 @@ class BinanceManager:
         trade = Trade(price, qty, ts_E)
         self.current_second_trades.append(trade)
 
+        for listener in self.msg_listeners:
+            listener(msg)
         for listener in self.trade_listeners:
             listener(trade)
         if old_second_trades:
             for listener in self.second_listeners:
                 listener(old_second, old_second_trades)
+
+
+class SaveMsgs:
+    def __init__(self):
+        self.msg_count = 0
+        self.out_file = open('msgs.txt', 'a')
+
+    def listener(self, msg):
+        self.out_file.write(json.dumps(msg) + '\n')
+        self.out_file.flush()
+        self.msg_count += 1
+        if self.msg_count % 100 == 0:
+            print(f'saved {self.msg_count} msgs')
